63fa3b038f6418a1185ac335498798ae925b4059
tekkoc <tekkoc@gmail.com>
2022-10-25 23:50:26 +0900

    引数を設定、読み込むように

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use argh::FromArgs;
+
+fn default_token_path() -> String {
+    String::from("~/.lino_token")
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// LINE Notify command.
+struct Args {
+    /// LINE Notify access token file path.
+    #[argh(option, short = 't', default = "default_token_path()")]
+    token: String,
+    #[argh(positional)]
+    message: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = argh::from_env();
+
+    fn read_stdin() -> String {
+        use std::io::stdin;
+        use std::io::Read;
+
+        let mut buffer = String::new();
+        stdin().read_to_string(&mut buffer).unwrap();
+        buffer
+    }
+
+    let message = match args.message {
+        Some(m) => m,
+        None => read_stdin(),
+    };
+
+    dbg!(message);
 }
